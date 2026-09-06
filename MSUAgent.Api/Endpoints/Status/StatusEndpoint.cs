87af69894b6dc5a5bebad3cf6f9cc5799c4bad98
Valeria <jamaisval@gmail.com>
2026-09-06 09:49:00 +0300

    Update status endpoint

---
+++ MSUAgent.Api/Endpoints/Status/StatusEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MSUAgent.Application.Queries;
+
+namespace MSUAgent.Api.Endpoints.Status
+{
+    public class StatusEndpoint
+    {
+        public static async Task<IResult> Handle(IMediator mediator)
+        {
+            var status = await mediator.Send(new GetStatusQuery());
+
+            return Results.Ok(status);
+        }
+    }
+}
--- MSUAgent.Api/Endpoints/Status/StatusEndpointGroup.cs
+++ MSUAgent.Api/Endpoints/Status/StatusEndpointGroup.cs
@@ -1,18 +1,10 @@
-﻿using MediatR;
-using MSUAgent.Application.Queries;
-
-namespace MSUAgent.Api.Endpoints.Status;
+﻿namespace MSUAgent.Api.Endpoints.Status;
 
 [ApiEndpointGroup("/status")]
 public sealed class StatusEndpointGroup : IEndpointGroup
 {
     public void MapEndpoints(RouteGroupBuilder group)
     {
-        group.MapGet("/", async (IMediator mediator) =>
-        {
-            var status = await mediator.Send(new GetStatusQuery());
-
-            return Results.Ok(status);
-        });
+        group.MapGet("/", StatusEndpoint.Handle);
     }
 }
