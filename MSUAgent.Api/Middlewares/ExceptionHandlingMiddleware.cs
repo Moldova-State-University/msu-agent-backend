8f56fcf5899eacba4ff8b757492f0c53a2137297
Valeria <jamaisval@gmail.com>
2026-09-14 13:43:24 +0300

    Add global exception handling middleware

---
+++ MSUAgent.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿namespace MSUAgent.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+
+    private async Task HandleExceptionAsync(
+        HttpContext context,
+        Exception exception)
+    {
+        _logger.LogError(
+            exception,
+            "An unhandled exception occurred.");
+
+        context.Response.StatusCode =
+            StatusCodes.Status500InternalServerError;
+
+        await context.Response.WriteAsJsonAsync(
+            new
+            {
+                message = "Internal server error"
+            });
+    }
+}
