8e1180f59aef1d041dd0c619a40d06fef12091c5
Valeria <jamaisval@gmail.com>
2026-09-23 11:51:36 +0300

    Add exception handling and Serilog logging to BFFs

---
+++ MSUAgent.MobileBff/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MSUAgent.MobileBff.Middlewares;
+
+namespace MSUAgent.MobileBff.Extensions;
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseMiddlewares(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+        return app;
+    }
+}
